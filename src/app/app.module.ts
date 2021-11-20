3d934b0ddecd726a96dc28b47ccd2c3dd70357db
Jonny Rojas del Río <jonnyr@nserio.com>
2021-11-20 07:25:14 -0500

    toolbar implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from '@app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@app/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RouterModule } from '@angular/router';
     BrowserAnimationsModule,
     RouterModule.forRoot([]),
     CoreModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
